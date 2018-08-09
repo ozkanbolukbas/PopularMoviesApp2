ef748e2f6c8babb62f769d6271ae6425e5c674f8
Rae <raemmacias@gmail.com>
2018-08-09 12:22:28 -0600

    Fixed recyclerview ID

--- app/src/main/java/movies/raemacias/com/movieappstage2/adapter/FavoriteAdapter.java
+++ app/src/main/java/movies/raemacias/com/movieappstage2/adapter/FavoriteAdapter.java
@@ -1,7 +1,5 @@
 package movies.raemacias.com.movieappstage2.adapter;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
